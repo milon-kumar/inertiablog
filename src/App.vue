3ca09ce3406c729272429ded695cb9e20c5d72b0
milon-kumar <mk81424291@gmail.com>
2022-06-04 17:57:58 +0600

    delete hellowworld

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <nav>
+    Navbar
+  </nav>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <router-view></router-view>
+
+  <footer>
+    Footer
+  </footer>
+</template>
